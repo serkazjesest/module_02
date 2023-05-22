e55cfc99750d4ab16c1065736e112f9a5f0a345b
serkazjesest <serkazjesest@gmail.com>
2023-05-22 12:28:45 +0300

    init task_07

--- main.go
+++ main.go
@@ -4,40 +4,69 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"time"
+	"strings"
 )
 
-var startTime = time.Now()
-
-func logTime() {
-	fmt.Println(time.Since(startTime))
-}
-
 func main() {
-	defer logTime()
 	in, err := os.Open("in.txt")
 	if err != nil {
-		fmt.Println("Эту обработку ошибок меня заставила делать Кашкина А.О. Файл короче не открывается.")
+		fmt.Println(err)
 		return
 	}
 	defer in.Close()
 
 	out, err := os.Create("out.txt")
 	if err != nil {
-		fmt.Println("Эту обработку ошибок меня заставила делать Кашкина А.О. Файл короче не создаётся.")
+		fmt.Println(err)
 		return
 	}
 	defer out.Close()
 
+	defer func() {
+		if panicErr := recover(); panicErr != nil {
+			switch panicErr {
+			case "empty field":
+				out, err := os.Open("out.txt")
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				defer out.Close()
+
+				s := bufio.NewScanner(out)
+				count := 0
+
+				for s.Scan() {
+					count++
+					fmt.Println(s.Text())
+				}
+				fmt.Println(fmt.Errorf("parse error: empty field on string %d", count))
+
+			default:
+				panic("critical")
+			}
+		}
+	}()
+
 	s := bufio.NewScanner(in)
-	count := 0
-	bytes := 0
+
+	line := 0
+	isEmpty := false
 	for s.Scan() {
-		count++
-		strForWrite := strconv.Itoa(count) + ". " + s.Text() + "\n"
-		bytes = bytes + len(strForWrite)
-		out.WriteString(strForWrite)
+		line++
+		workStr := s.Text()
+		workSlice := strings.SplitN(workStr, "|", 3)
+		for _, s := range workSlice {
+			if s == "" {
+				isEmpty = true
+			}
+		}
+		if !isEmpty {
+			workStr = fmt.Sprintf("Row: %d\nName: %s\nAddress: %s\nCity: %s\n\n\n", line, workSlice[0], workSlice[1], workSlice[2])
+			out.WriteString(workStr)
+
+		} else {
+			panic("empty field")
+		}
 	}
-	defer fmt.Print("Количество строк записанных в файл:", count, "\n", "Количество байт записанных в файл:", bytes, "\n")
 }
